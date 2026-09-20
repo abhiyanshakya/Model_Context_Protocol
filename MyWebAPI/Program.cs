0744a5fe3ae93f108961655628feac089ce072c3
abhiyanshakya <abhiyanshakya64@gmail.com>
2026-09-20 20:57:11 +0930

    Add Swagger docs

--- MyWebAPI/Program.cs
+++ MyWebAPI/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 *-----------------ASP.NET services our application use-----------------
 */
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 //registering and configure an httpclient.
 builder.Services.AddHttpClient<ITodoService, TodosService>(client =>
@@ -18,6 +19,13 @@ builder.Services.AddHttpClient<ITodoService, TodosService>(client =>
 });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+// You can remove this check if you want Swagger available in staging/production environment.
+{
+    app.UseSwagger();   // Serves the generated OpenAPI spec file as a JSON endpoint
+    app.UseSwaggerUI(); // Serves the interactive web UI
+}
 
 app.UseHttpsRedirection();
 
